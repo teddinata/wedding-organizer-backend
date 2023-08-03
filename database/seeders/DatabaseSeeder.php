c2c67a3db3890fc53a3956a5425e57918551b2db
angelasherly <59503679+angelasherly@users.noreply.github.com>
2023-08-03 14:29:01 +0700

    added decoration area seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,5 +27,6 @@ class DatabaseSeeder extends Seeder
         $this->call(VendorGradesTableSeeder::class);
         $this->call(VendorLimitsTableSeeder::class);
         $this->call(VendorMembershipsTableSeeder::class);
+        $this->call(DecorationAreaTableSeeder::class);
     }
 }
---
+++ database/seeders/DecorationAreaTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DecorationAreaTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        DB::table('decoration_area')->insert([
+            [
+                'name' => 'Stage Area',
+                'created_at' => now(),
+            ],
+            [
+                'name' => 'Ballroom Area',
+                'created_at' => now(),
+            ],
+            [
+                'name' => 'Foyer Area',
+                'created_at' => now(),
+            ],
+            [
+                'name' => 'Lighting',
+                'created_at' => now(),
+            ]
+            // Tambahkan data lain sesuai kebutuhan
+        ]);
+    }
+}
