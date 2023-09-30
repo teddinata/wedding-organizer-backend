f995f1bd71e70e68806b70ee05c0ff8aebf8386a
angelasherly <59503679+angelasherly@users.noreply.github.com>
2023-09-30 11:03:55 +0700

    added condition not found in search function and remove activity log from method get and show

--- app/Http/Controllers/Api/Web/DepartmentController.php
+++ app/Http/Controllers/Api/Web/DepartmentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API\Web;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Spatie\Activitylog\Models\Activity;
 // use resource
@@ -18,41 +19,37 @@ class DepartmentController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        // get all department with filter and pagination
-        $department = Department::orderBy('name', 'asc');
-
-        // filter by name and description in one search field
-        if (request()->has('search')) {
-            $department->where(function ($q) {
-                $q->where('name', 'like', '%' . request('search') . '%');
-            });
-        }
-
-
         // Get pagination settings
         $perPage = request('per_page', 10);
         $page = request('page', 1);
 
-        // get department data and sort by name ascending
-        $department = $department->paginate($perPage, ['*'], 'page', $page);
+        //set variable for search
+        $search = $request->query('search');
 
-        // Log Activity
-        Activity::create([
-            'log_name' => 'Show Data',
-            'description' => 'User ' . Auth::user()->name . ' Show department list',
-            'subject_id' => Auth::user()->id,
-            'subject_type' => 'App\Models\User',
-            'causer_id' => Auth::user()->id,
-            'causer_type' => 'App\Models\User',
-            'properties' => request()->ip(),
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
+        //set condition if search not empty then search by account_holder or account_number else then show all data
+        if (!empty($search)) {
+            $query = Department::where('name', 'like', '%' . $search . '%')
+                ->paginate(
+                    $perPage,
+                    ['*'],
+                    'page',
+                    $page
+                );
+
+            //check result
+            $recordsTotal = $query->count();
+            if (empty($recordsTotal)) {
+                return response(['Message' => 'Data not found!'], 404);
+            }
+        } else {
+            // get bank account data and sort by account_holder ascending
+            $query = Department::orderBy('name', 'asc')->paginate($perPage, ['*'], 'page', $page);
+        }
 
         //return collection of department as a resource
-        return new DepartmentResource(true, 'Department retrieved successfully', $department);
+        return new DepartmentResource(true, 'Department retrieved successfully', $query);
     }
 
     /**
@@ -61,7 +58,7 @@ class DepartmentController extends Controller
     public function store(StoreDepartmentRequest $request)
     {
         //store to database
-        $department = Department::create([
+        $query = Department::create([
             'name' => $request->name,
             'payroll_type' => $request->payroll_type,
             'is_has_schedule' => $request->is_has_schedule,
@@ -72,8 +69,8 @@ class DepartmentController extends Controller
 
         // activity log
         Activity::create([
-            'log_name' => 'Department Creation',
-            'description' => 'User ' . Auth::user()->name . ' create department ' . $department->name,
+            'log_name' => 'User ' . Auth::user()->name . ' add new department',
+            'description' => 'User ' . Auth::user()->name . ' create new department ' . $query->name,
             'subject_id' => Auth::user()->id,
             'subject_type' => 'App\Models\User',
             'causer_id' => Auth::user()->id,
@@ -83,7 +80,7 @@ class DepartmentController extends Controller
         ]);
 
         // return json response
-        return new DepartmentResource(true, $department->name . ' has successfully been created.', $department);
+        return new DepartmentResource(true, $query->name . ' has successfully been created.', $query);
     }
 
     /**
@@ -91,22 +88,11 @@ class DepartmentController extends Controller
      */
     public function show($id)
     {
-        $department = Department::findOrFail($id);
-        //return single post as a resource
-        return new DepartmentResource(true, 'Data Department Found!', $department);
+        // find the data by ID
+        $query = Department::findOrFail($id);
 
-        // activity log
-        Activity::create([
-            'log_name' => 'View Data',
-            'description' => 'User ' . Auth::user()->name . ' view department ' . $department->name,
-            'subject_id' => Auth::user()->id,
-            'subject_type' => 'App\Models\User',
-            'causer_id' => Auth::user()->id,
-            'causer_type' => 'App\Models\User',
-            'properties' => request()->ip(),
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
+        //return single post as a resource
+        return new DepartmentResource(true, 'Department found!', $query);
     }
 
     /**
@@ -114,11 +100,11 @@ class DepartmentController extends Controller
      */
     public function update(UpdateDepartmentRequest $request, $id)
     {
-        // find the data
-        $department = Department::findOrFail($id);
+        // find the data by ID
+        $query = Department::findOrFail($id);
 
         // update to database
-        $department->update(($request->validated() + [
+        $query->update(($request->validated() + [
             'name' => $request->name,
             'payroll_type' => $request->payroll_type,
             'is_has_schedule' => $request->is_has_schedule,
@@ -129,8 +115,8 @@ class DepartmentController extends Controller
 
         // activity log
         Activity::create([
-            'log_name' => 'Update Data',
-            'description' => 'User ' . Auth::user()->name . ' update department to ' . $department->name,
+            'log_name' => 'User ' . Auth::user()->name . ' update department information',
+            'description' => 'User ' . Auth::user()->name . ' update department to ' . $query->name,
             'subject_id' => Auth::user()->id,
             'subject_type' => 'App\Models\User',
             'causer_id' => Auth::user()->id,
@@ -141,7 +127,7 @@ class DepartmentController extends Controller
         ]);
 
         // return json response
-        return new DepartmentResource(true, $department->name . ' has successfully been updated.', $department);
+        return new DepartmentResource(true, $query->name . ' has successfully been updated.', $query);
     }
 
     /**
@@ -149,17 +135,17 @@ class DepartmentController extends Controller
      */
     public function destroy($id)
     {
-        // find data
-        $department = Department::findOrFail($id);
-        $department->delete();
+        // find data by ID
+        $query = Department::findOrFail($id);
+        $query->delete();
         // soft delete to database
-        $department->deleted_by = Auth::user()->id;
-        $department->save();
+        $query->deleted_by = Auth::user()->id;
+        $query->save();
 
         // activity log
         Activity::create([
             'log_name' => 'Delete Data',
-            'description' => 'User ' . Auth::user()->name . ' delete department ' . $department->name,
+            'description' => 'User ' . Auth::user()->name . ' delete department ' . $query->name,
             'subject_id' => Auth::user()->id,
             'subject_type' => 'App\Models\User',
             'causer_id' => Auth::user()->id,
@@ -169,6 +155,6 @@ class DepartmentController extends Controller
         ]);
 
         // return json response
-        return new DepartmentResource(true, $department->name . ' has successfully been deleted.', null);
+        return new DepartmentResource(true, $query->name . ' has successfully been deleted.', null);
     }
 }
